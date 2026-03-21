6ee1cbbfb35b4c852fe0e2d966d81d2d5fc00a6a
WillowWisp <ckurschat@aol.com>
2026-03-21 15:23:04 -0400

    v1.1.5 fixed gourmand intro

--- main/src/hooks/SlugcatStatsMixin.cs
+++ main/src/hooks/SlugcatStatsMixin.cs
@@ -1,4 +1,5 @@
 ﻿using Myriad.utils;
+using UnityEngine;
 
 namespace Myriad.hooks; 
 
@@ -10,9 +11,34 @@ public class SlugcatStatsMixin {
         On.SlugcatStats.Name.ArenaColor += Name_ArenaColor;
         On.SlugcatStats.Name.Init += Name_Init;
         On.SlugcatStats.HiddenOrUnplayableSlugcat += SlugcatStats_HiddenOrUnplayableSlugcat;
+        On.MoreSlugcats.MSCRoomSpecificScript.SH_GOR02.ctor += SH_GOR02_ctor; //FIX THE GOURM INTRO NOT HAVING ENOUGH SPAWN SPOTS
     }
-    
-     //ADDING FAKE CHARACTERS FOR THE ARENA MODE COLORS
+
+    private void SH_GOR02_ctor(On.MoreSlugcats.MSCRoomSpecificScript.SH_GOR02.orig_ctor orig, MoreSlugcats.MSCRoomSpecificScript.SH_GOR02 self, Room room) {
+        orig(self, room);
+        self.spawnSpots = new Vector2[]
+        {
+            new Vector2(450f, 170f),
+            new Vector2(690f, 230f),
+            new Vector2(730f, 230f),
+            new Vector2(230f, 310f),
+            //JUST LOOP IT A BUNCH MORE
+            new Vector2(450f, 170f),
+            new Vector2(690f, 230f),
+            new Vector2(730f, 230f),
+            new Vector2(230f, 310f),
+            new Vector2(450f, 170f),
+            new Vector2(690f, 230f),
+            new Vector2(730f, 230f),
+            new Vector2(230f, 310f),
+            new Vector2(450f, 170f),
+            new Vector2(690f, 230f),
+            new Vector2(730f, 230f),
+            new Vector2(230f, 310f)
+        };
+    }
+
+    //ADDING FAKE CHARACTERS FOR THE ARENA MODE COLORS
     private void Name_Init(On.SlugcatStats.Name.orig_Init orig) {
         orig();
         // ExtEnum<SlugcatStats.Name>.values.AddEntry(SlugcatStats.Name.White.value);
@@ -35,9 +61,21 @@ public class SlugcatStatsMixin {
     private bool SlugcatStats_HiddenOrUnplayableSlugcat(On.SlugcatStats.orig_HiddenOrUnplayableSlugcat orig, SlugcatStats.Name i) {
         bool extraPlayer = ExtraSlugcatNames.isExtraName(i);
         
+        //MAYBE ANOTHER TIME
+        //if (i == Watcher.WatcherEnums.SlugcatStatsName.Watcher || (ModManager.MSC && i == MoreSlugcats.MoreSlugcatsEnums.SlugcatStatsName.Sofanthiel)) {
+        //    return false;
+        //}
         return orig(i) || extraPlayer;
     }
-    
+
+    //MAYBE ANOTHER TIME...
+    /*private bool SlugcatStats_SlugcatUnlocked(On.SlugcatStats.orig_SlugcatUnlocked orig, SlugcatStats.Name i, RainWorld rainWorld) {
+        if (i == Watcher.WatcherEnums.SlugcatStatsName.Watcher || (ModManager.MSC && i == MoreSlugcats.MoreSlugcatsEnums.SlugcatStatsName.Sofanthiel)) {
+            return true;
+        }
+        return orig(i, rainWorld);
+    }*/
+
     private SlugcatStats.Name Name_ArenaColor(On.SlugcatStats.Name.orig_ArenaColor orig, int playerIndex) {
         //THIS VERSION WORKS EVEN IF MSC IS NOT ENABLED
         SlugcatStats.Name? name = playerIndex switch {
